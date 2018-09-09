574c18d4e7f1bd66772af16fa17387893f42572f
kevinrao <kevinrao2000@gmail.com>
2018-09-09 03:04:22 -0400

    bug fixing

--- compendium/views.py
+++ compendium/views.py
@@ -5,13 +5,14 @@ from .models import Hospital
 
 # Create your views here.
 def index(request):
-    hospital_list = Hospital.objects.raw('''SELECT id AS idnum, name AS name, CONCAT(address, city, state, zipcode) AS address, phonenumber AS phonenumber, hospitaltype AS hospitaltype, emergency AS emergency, criteria AS meetcriteria,  overallrating AS overallrating FROM general''')
+    hospital_list = Hospital.objects.raw('''SELECT id AS idnum, name AS name, CONCAT(address, ' ', city, ' ', state, ' ', zipcode) AS address, phonenumber AS phonenumber, hospitaltype AS hospitaltype, emergency AS emergency, criteria AS meetcriteria,  overallrating AS overallrating FROM general''')
+    hospital_info = []
     for a in hospital_list:
-        print(a.name.title())
+        hospital_info.append([a.idnum, a.name.title(), a.address.title(), a.phonenumber, a.hospitaltype, a.overallrating])
     template = loader.get_template("index.html")
     context = {
-        'hospital_list' : hospital_list
+        'hospital_info' : hospital_info
     }
     return HttpResponse(template.render(context, request))
 
-def details(request):
+#def details(request):
